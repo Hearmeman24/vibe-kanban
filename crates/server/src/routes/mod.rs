1cde918e52ddd0b4dadcbbeb7b934b53c244d3c5
avivkaplan <avivkaplan00@gmail.com>
2026-01-04 19:19:45 +0200

    auto: update mod.rs [19:19:45]

--- crates/server/src/routes/mod.rs
+++ crates/server/src/routes/mod.rs
@@ -39,6 +39,7 @@ pub fn router(deployment: DeploymentImpl) -> IntoMakeService<Router> {
         .merge(task_attempts::router(&deployment))
         .merge(execution_processes::router(&deployment))
         .merge(tags::router(&deployment))
+        .merge(task_comments::router(&deployment))
         .merge(oauth::router())
         .merge(organizations::router())
         .merge(filesystem::router())
