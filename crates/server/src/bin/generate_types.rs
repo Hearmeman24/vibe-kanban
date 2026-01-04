2d28f10b2a35fad7dff12ba22ac9c49ce72f2196
avivkaplan <avivkaplan00@gmail.com>
2026-01-04 21:30:36 +0200

    auto: update generate_types.rs [21:30:36]

--- crates/server/src/bin/generate_types.rs
+++ crates/server/src/bin/generate_types.rs
@@ -37,6 +37,7 @@ fn generate_types_content() -> String {
         db::models::task::TaskRelationshipsSimple::decl(),
         db::models::task::CreateTask::decl(),
         db::models::task::UpdateTask::decl(),
+        db::models::task::AgentMetadataEntry::decl(),
         db::models::scratch::DraftFollowUpData::decl(),
         db::models::scratch::ScratchPayload::decl(),
         db::models::scratch::ScratchType::decl(),
