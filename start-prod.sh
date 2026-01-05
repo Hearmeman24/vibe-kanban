e80dffd406e631d755e2bbc31feb59811be9efce
avivkaplan <avivkaplan00@gmail.com>
2026-01-05 09:08:56 +0200

    auto: update start-prod.sh [09:08:56]

--- start-prod.sh
+++ start-prod.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
+PROJECT_DIR="/Users/avivkaplan/src/vibe-kanban"
+cd "$PROJECT_DIR"
+
 echo "=== Building Frontend ==="
 cd frontend
 pnpm install
 pnpm build
-cd ..
+cd "$PROJECT_DIR"
 
 echo "=== Building Rust Server (Release) ==="
 cargo build --release
