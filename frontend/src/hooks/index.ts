dd7fc7c4f9ee838353ffb0b02d2beaebee6bc36d
avivkaplan <avivkaplan00@gmail.com>
2026-01-06 23:47:24 +0200

    auto: update index.ts [23:47:24]

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -32,3 +32,5 @@ export { useOrganizationInvitations } from './useOrganizationInvitations';
 export { useOrganizationMutations } from './useOrganizationMutations';
 export { useVariant } from './useVariant';
 export { useRetryProcess } from './useRetryProcess';
+export { useProjectAgents, projectAgentKeys } from './useProjectAgents';
+export { useGlobalAgents, globalAgentKeys } from './useGlobalAgents';
